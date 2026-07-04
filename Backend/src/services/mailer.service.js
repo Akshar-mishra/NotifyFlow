d143744900a6e40e886a458e8b624db13fcff952
Akshar-mishra <mishraakshar71@gmail.com>
2026-07-05 01:25:10 +0530

    update sender email address in mailer service

--- Backend/src/services/mailer.service.js
+++ Backend/src/services/mailer.service.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 export const sendEmail = async (to, subject, body) => {
   try {
     const info = await resend.emails.send({
-      from: 'NotifyFlow <mishraakshar71@gmail.com>',
+      from: 'NotifyFlow <onboarding@resend.dev>',
       to,
       subject,
       text: body
