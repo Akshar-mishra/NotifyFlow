a5269c57d03fd4d0367d2dcfea0345b496f6fea6
Akshar-mishra <mishraakshar71@gmail.com>
2026-07-05 01:15:27 +0530

    changing mail service frm nodemailer to resend

--- Backend/src/services/mailer.service.js
+++ Backend/src/services/mailer.service.js
@@ -1,32 +1,19 @@
-import nodemailer from 'nodemailer'   
+import { Resend } from 'resend'
 
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
-    }
-})
-transporter.verify((err, success) => {
-  if (err) console.error("SMTP verify failed:", err)
-  else console.log("SMTP server ready")
-})
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const sendEmail = async (to, subject, body) => {
-    try {
-        const info = await transporter.sendMail({
-            from: `"NotifyFlow" <${process.env.SMTP_USER}>`, // Sender address
-            to: to,                                          // Receiver address
-            subject: subject,                                // Subject line
-            text: body                                       // Plain text body
-        })   
-        
-        return info   
-    } 
-    catch (error) {
-        console.error("Nodemailer Error:", error)   
-        throw error  
-    }
-}   
+  try {
+    const info = await resend.emails.send({
+      from: 'NotifyFlow <mishraakshar71@gmail.com>',
+      to,
+      subject,
+      text: body
+    })
+    return info
+  } 
+  catch (error) {
+    console.error("Resend Error:", error)
+    throw error
+  }
+}
